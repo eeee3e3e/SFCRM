3e31ad8f9c2caff261a8f7479d74e39a9c8ab9f1
118201387526 <925066417@qq.com>
2016-07-09 13:46:31 +0800

    0709
    0709

---
+++ ShunFengCRM.DTO/VisitReportModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShunFengCRM.DTO
+{
+    public class VisitReportModel
+    {
+        public int Id { get; set; }
+
+        public string ClientName { get; set; }
+
+        public string MonthlyAccount { get; set; }
+
+        public decimal ProAccount { get; set; }
+
+        public int ProductID { get; set; }
+
+        public int ProfesionID { get; set; }
+
+        public int KindID { get; set; }
+
+        public int PhraseID { get; set; }
+
+        public int StaffID { get; set; }
+
+        public DateTime VisitDate { get; set; }
+
+        public int RqID { get; set; }
+
+        public string Remark { get; set; }
+
+    }
+}
