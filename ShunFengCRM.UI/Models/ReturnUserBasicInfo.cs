c4d88f860c8fd6c1a2a080a0698ba7461baef06a
eeee3e3e <yy_double_y@126.com>
2016-07-10 12:56:10 +0800

    0710

---
+++ ShunFengCRM.UI/Models/ReturnUserBasicInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShunFengCRM.UI.Models
+{
+    public class UserBasicInfo
+    {
+        public int NickName { get; set; }
+
+        public string StaffNo { get; set; }
+
+        public string OrgCode { get; set; }
+
+        public int OrgName { get; set; }
+    }
+}
